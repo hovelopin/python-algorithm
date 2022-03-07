75e6d64b5dda5d0e5dff0380c4b68f5f5b731530
hovelopin <kimhojin3714@naver.com>
2022-03-07 20:39:39 +0900

    BOJ 3/7

--- BOJ/implementation/b2331p.py
+++ BOJ/implementation/b2331p.py
@@ -1,1 +1,19 @@
-A , P = map(int,input().split())
+A , P = map(str , input().split())
+
+AList = []
+
+AList.append(A)
+
+while 1:
+    result = 0
+    for i in AList[-1]:
+        result +=int(i)**int(P)
+    AList.append(str(result))
+    if(AList.count(AList[-1]) > 1):
+        idx = AList.index(AList[-1])
+        resultList = AList[:idx]
+        break
+print(len(resultList))
+
+
+
