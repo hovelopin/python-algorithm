b5988798a130eb4a503bf329fbaaf1aaae9387cc
hovelopin <kimhojin3714@naver.com>
2022-08-11 23:28:39 +0900

    example

--- BOJ/newIndex.py
+++ BOJ/newIndex.py
@@ -1,3 +1,15 @@
-a=set([1,2,1,1,3])
-b = list(a)
-print(b)
+n = int(input())
+
+str = '*'
+start = 1
+
+def solution(num):
+    global start
+    if num == 1:
+        return print(str * start)
+    print(str * start)
+    start += 1
+
+    return solution(num-1);
+
+solution(n)
